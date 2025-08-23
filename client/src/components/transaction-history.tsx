2332cb2df46ddeaa7d2cb1a984af6f52969a7e9c
snaik0520 <113665150+snaik0520@users.noreply.github.com>
2025-08-23 16:09:33 -0700

    Update transaction-history.tsx

--- client/src/components/transaction-history.tsx
+++ client/src/components/transaction-history.tsx
@@ -61,7 +61,6 @@ export function TransactionHistory() {
   const getTransactionTitle = (type: string) => {
     switch (type) {
       case "addition":
-      case "added":  // Add this line
         return "Added";
       case "dispensed":
         return "Dispensed";
