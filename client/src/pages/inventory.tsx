a4c4be50183f97bb763372566524d1fd8b707814
snaik0520 <113665150+snaik0520@users.noreply.github.com>
2025-09-02 12:47:44 -0700

    Update inventory.tsx

--- client/src/pages/inventory.tsx
+++ client/src/pages/inventory.tsx
@@ -357,10 +357,10 @@ export default function Inventory() {
                             </span>
                           </td>
                           <td className="px-6 py-4 whitespace-nowrap text-center text-sm text-gray-500">
-                            <div className="max-w-96 break-words whitespace-normal leading-tight text-sm">
-                              <div className="break-words max-w-[120px]">{medication.location ?? "—"}</div>
-                            </div>
-                          </td>
+                            <div className="break-words max-w-[120px]">
+    {medication.location ?? "—"}
+  </div>
+</td>
                           <td className="px-6 py-4 whitespace-nowrap text-center">
                             <Button
                               onClick={() => handleDispense(medication)}
