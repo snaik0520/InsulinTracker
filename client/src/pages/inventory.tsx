11cdf494fa48668090fdfe2f5c2563d9f10523e4
snaik0520 <113665150+snaik0520@users.noreply.github.com>
2025-09-02 14:03:25 -0700

    Update inventory.tsx

--- client/src/pages/inventory.tsx
+++ client/src/pages/inventory.tsx
@@ -356,7 +356,7 @@ export default function Inventory() {
                             </span>
                           </td>
                           <td className="px-6 py-4 align-top text-sm text-gray-500">
-                            <div className="w-[180px] max-w-full break-words whitespace-normal leading-tight text-sm text-left">
+                            <div className="w-[120px] max-w-full break-words whitespace-normal leading-tight text-sm text-left">
                               {medication.location ?? "—"}
                             </div>
                           </td>
